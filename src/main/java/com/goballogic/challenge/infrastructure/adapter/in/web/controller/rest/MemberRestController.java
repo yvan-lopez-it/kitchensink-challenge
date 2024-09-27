5a09328ebc3e5740f71b102c4282df7b31b934d2
Yvan Lopez <ylopez@getontop.com>
2024-09-27 16:13:49 -0500

    new columns for rest calls.

--- src/main/java/com/goballogic/challenge/infrastructure/adapter/in/web/controller/rest/MemberRestController.java
+++ src/main/java/com/goballogic/challenge/infrastructure/adapter/in/web/controller/rest/MemberRestController.java
@@ -2,7 +2,10 @@ package com.goballogic.challenge.infrastructure.adapter.in.web.controller.rest;
 
 import com.goballogic.challenge.application.port.in.MemberUseCase;
 import com.goballogic.challenge.infrastructure.adapter.in.dto.MemberDTO;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,20 @@ public class MemberRestController {
     public ResponseEntity<MemberDTO> registerMember(@RequestBody MemberDTO memberDTO) {
         MemberDTO savedMember = memberUseCase.registerMember(memberDTO);
         return ResponseEntity.ok(savedMember);
+    }
+
+    @GetMapping("/members/{id}")
+    public ResponseEntity<MemberDTO> getMemberById(@PathVariable String id) {
+        MemberDTO member = memberUseCase.getMemberById(id).orElse(null);
+        if (member == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(member);
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<List<MemberDTO>> getAllMembers() {
+        List<MemberDTO> members = memberUseCase.getAllMembers();
+        return ResponseEntity.ok(members);
     }
 }
