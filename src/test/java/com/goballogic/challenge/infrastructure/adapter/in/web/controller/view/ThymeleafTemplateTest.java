18dc71b18f2c4d6a484917bbfac1d555bbaffa87
Yvan Lopez <ylopez@getontop.com>
2024-09-30 19:37:49 -0500

    ThymeleafTemplateTest updated.

--- src/test/java/com/goballogic/challenge/infrastructure/adapter/in/web/controller/view/ThymeleafTemplateTest.java
+++ src/test/java/com/goballogic/challenge/infrastructure/adapter/in/web/controller/view/ThymeleafTemplateTest.java
@@ -2,6 +2,7 @@ package com.goballogic.challenge.infrastructure.adapter.in.web.controller.view;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,6 +27,15 @@ class ThymeleafTemplateTest {
             .andExpect(content().string(containsString("Name")))
             .andExpect(content().string(containsString("Email")))
             .andExpect(content().string(containsString("Phone #")));
+    }
+
+    @Test
+    void testInvalidMemberForm() throws Exception {
+        mockMvc.perform(post("/rest/register")
+                .param("name", "Juan123")
+                .param("email", "invalid-email")
+                .param("phoneNumber", "12345"))
+            .andExpect(status().isBadRequest());
     }
 
 }
